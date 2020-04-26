9e32f05a79621ca847d59719136a4ad5ca471d02
Rocio Soledad Segovia Hoyos <rsegovia347@gmail.com>
2020-04-26 02:27:49 -0300

    add products controller

---
+++ shop.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace shop.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ shop.web/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿namespace shop.web.Data
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+    }
+}
